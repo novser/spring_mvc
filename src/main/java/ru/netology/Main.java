4f52b0ba2910cf7f9891697521f64dd301c16e56
Sergey Novikov <vizzeb@gmail.com>
2022-08-15 17:16:46 +0300

    final

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,20 +8,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Main {
-  public static void main(String[] args) throws LifecycleException, IOException {
-    final var tomcat = new Tomcat();
-    final var baseDir = Files.createTempDirectory("tomcat");
-    baseDir.toFile().deleteOnExit();
-    tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
+    public static void main(String[] args) throws LifecycleException, IOException {
+        final var tomcat = new Tomcat();
+        final var baseDir = Files.createTempDirectory("tomcat");
+        baseDir.toFile().deleteOnExit();
+        tomcat.setBaseDir(baseDir.toAbsolutePath().toString());
 
-    final var connector = new Connector();
-    connector.setPort(9999);
-    tomcat.setConnector(connector);
+        final var connector = new Connector();
+        connector.setPort(9999);
+        tomcat.setConnector(connector);
 
-    tomcat.getHost().setAppBase(".");
-    tomcat.addWebapp("", ".");
+        tomcat.getHost().setAppBase(".");
+        tomcat.addWebapp("", ".");
 
-    tomcat.start();
-    tomcat.getServer().await();
-  }
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
